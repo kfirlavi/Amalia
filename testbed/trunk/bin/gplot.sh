ab6dbe14be88a22db7389b03cae00fbe603ca74b
Kfir Lavi <lavi.kfir@gmail.com>
2008-02-15 09:19:04 +0000

    added the ability to specify a size of plot. Adding to the url at the end ?size=[normal|big|huge].

--- testbed/trunk/bin/gplot.sh
+++ testbed/trunk/bin/gplot.sh
@@ -11,11 +11,19 @@ PLOTNUM=`echo "$QUERY_STRING" | sed -n 's/^.*plot=\([^&]*\).*$/\1/p' | sed "s/%2
 TITLE=`echo "$QUERY_STRING" | sed -n 's/^.*title=\([^&]*\).*$/\1/p' | sed "s/%20/ /g"`
 XHI=`echo "$QUERY_STRING" | sed -n 's/^.*xhi=\([^&]*\).*$/\1/p' | sed "s/%20/ /g"`
 STYLE=`echo "$QUERY_STRING" | sed -n 's/^.*style=\([^&]*\).*$/\1/p' | sed "s/%20/ /g"`
+SIZE=`echo "$QUERY_STRING" | sed -n 's/^.*size=\([^&]*\).*$/\1/p' | sed "s/%20/ /g"`
 if [[ $FORMAT = "eps" ]]; then
   FORMAT="postscript eps enhanced color"
   MIME="Application/PostScript"
-else
+fi
+if [[ $FORMAT = "" ]]; then
   FORMAT="png"
+  case $SIZE in
+    normal) FORMAT="png size 1200,800";;
+    big) FORMAT="png size 2400,1600";;
+    huge) FORMAT="png size 3200,2400";;
+    *) FORMAT="png";;
+  esac
   MIME="image/png"
 fi
 
