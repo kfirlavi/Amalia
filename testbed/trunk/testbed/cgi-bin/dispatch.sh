cf1fe16f5879f5cf6be55d7f1134d7424464e868
Kfir Lavi <lavi.kfir@gmail.com>
2008-03-26 20:00:22 +0000

    added the ability to ignore gz ending and catch the probe,ipref,dump ending for a file

--- testbed/trunk/testbed/cgi-bin/dispatch.sh
+++ testbed/trunk/testbed/cgi-bin/dispatch.sh
@@ -5,9 +5,23 @@ function print_error_page()
 	echo Content-type: text/plain
 	echo ""
 	echo "Error: $1" 
+	echo "FILE_TYPE=$FILE_TYPE"
+	echo "PATH_TRANSLATED=$PATH_TRANSLATED"
 }
 
-FILE_TYPE=`echo $PATH_TRANSLATED | sed 's/\(.*\)\.\(.*\)/\2/'`
+function get_file_type()
+{
+	FILE_TYPE=`echo $PATH_TRANSLATED | sed 's/\(.*\)\.\(.*\)/\2/'`
+
+	# if the file type is 'gz', then we need the type before (probe.gz ie. probe)
+	if [[ $FILE_TYPE == "gz" ]]
+	then
+		FILE_TYPE=`echo $PATH_TRANSLATED | sed 's/\(.*\)\.\(.*\)\.gz/\2/'`
+	fi
+	return $FILE_TYPE
+}
+
+FILE_TYPE= get_file_type
 case "$FILE_TYPE" in
 	probe)
 		$PWD/gplot.probe.sh
