8d3f39398836c7c31fd4bd779f956f89e669429f
Ian Dangerfield <ian.dangerfield@gmail.com>
2008-05-26 15:59:29 +0000

    Cleaned up version of process.delays.c. will print out statistics by ratecode instead of packet size, or all packets when ratecode argument set to 0

--- madwifi/util/process.delays/process.delays.c
+++ madwifi/util/process.delays/process.delays.c
@@ -1,52 +1,56 @@
 
 #include<stdio.h>
-#include<stdlib.h> // for malloc()
+#include<stdlib.h> // for atoi()
 
 // #define P_SIZE 140	// voice packet size (output of bf->bf_skb->size in dmesg)
 			// 140 for 80byte, 1060 for 1000byte and 1530 for 1470byte
-#define T_MIN 100 	// start at 100 to save some space (292 theoretical min)
+#define T_MIN 200 	// start at 100 to save some space (292 theoretical min)
 #define M_BIN_NUM 10000  // Number of bins
 #define BIN_DIV	10	// Increment for the bin size
+#define WRAP_TIME	4294967295	// size of 2^32-1 for wrap around time
 // #define REAL_P_SIZE 80  // Real size of packet (data only, no headers)
 			// 80 for voice, 1000 or 1470 for the data packets
-// #define WRAP_32BIT_NUM 268435456 // 28bit tsf ?
+
 
 FILE *f_input, *f_out, *f_out_dist, *f_average, *f_out_percent;
 
 // Data structure for storing start and end times ids
 
+
 typedef struct{
-	int s_id;
-	int e_id;
-	int e_size;	
-	double s_usec;
-	double e_usec;
-    int retries;
-    int rate;
+	int id;                   // Hardware ID
+	unsigned int return_t_usec;     // timestamp after ACK or dropped for excessive retries
+    int total_queued;               // total packets ever queued
+	unsigned int queuing_t_usec;    // unsigned timestamps
+	int packet_size;	        // size of the packet buffer (packet + driver stuff...)
+	int status;               // status of packet, 0= sucessful TX, 1= dropped
+	int ratecode;             // ratecode for rate packet was sent at
+    int queue_depth;                // number of packets
+	int lretry;               // number of (long) retries
+    int rssi;                 // RSSI of recieved ACK
 }data_line;
 
 int main(int argc, char *argv[])
 {
 int num_sta=0;
-double run_time=0, start_time=0;
+double run_time=0., start_time=0. ,last_wrap_time=0.;
 char *in_file, *out_file, *ave_file, *dist_file, *percent_file;
-int total_num_packets=0, wrap_counter=0;
-//int s_sec, s_usec, e_sec, e_usec;
+int total_num_packets=0;
 int i=0, j=0;
 data_line current, last;
 int min_delay=-1; //min delay,  for comparsion to theoretical min. =99! NOT = 0!!
 int data_bin[M_BIN_NUM][3], bin_num=0, median=-1; //for getting median
-double pop_temp=0, percent_temp=0; //Dummy variables for calculations
-double percentage=0; //For percentage distribution calculation
-double delay=0, throughput=0, service_average=0, queuing_average=0;
-int p_size, real_p_size;
-int qt=0, qd=0, status=0, rssi=0; //dummy variables for fscanf
+double pop_temp=0., percent_temp=0.; //Dummy variables for calculations
+double percentage=0.; //For percentage distribution calculation
+double delay=0., throughput=0., service_average=0., queue_average=0., col_prob=0.;
+int p_size, real_p_size, total_l_retries=0, time_wrap_counter=0;
+int desired_ratecode=-1; //can pick packets via rate code
 
 	/*
 	 * File management
 	 */ 
 	if(argc != 8){
-		printf("%s: <f_input> <f_out> <f_average> <f_dist> <f_percent> <num sta> <p_size>\n", argv[0]);
+		printf("%s: <f_input> <f_out> <f_average> <f_dist> <f_percent> <num sta> <rate_code>\n", argv[0]);
 		exit(1);
 	}
 	//Files
@@ -57,13 +61,9 @@ int qt=0, qd=0, status=0, rssi=0; //dummy variables for fscanf
 	percent_file =argv[5];
 	//Variables
 	num_sta = atoi(argv[6]);
-	//num_sta is a dummy variable used it index the results
-	//it is not used to calculate anything.
-	real_p_size = atoi(argv[7]);
-	//p_size = real_p_size + 60;
-	p_size = real_p_size;
-
-
+	//real_p_size = atoi(argv[7]);
+	desired_ratecode = atoi(argv[7]);
+    
 	f_input = fopen(in_file,"r");
 	f_out = fopen(out_file, "w");
 	f_average = fopen(ave_file, "a"); //append to this file
@@ -87,63 +87,47 @@ int qt=0, qd=0, status=0, rssi=0; //dummy variables for fscanf
 	 * Here we store 2 lines of data, calculate delays and
 	 * populate the bins. Averages are left until afterwards
 	 */ 
-	last.s_id = -1;
-	last.e_id = -1;	
+	last.id = -1;
+	last.id = -1;	
 	service_average = 0;
-	queuing_average = 0;
+	queue_average = 0;
 	min_delay = -1;
 	total_num_packets=0;
-    /*
-     * Format has changed 
-     * IS_TIME driver update prints out in the following format
-     * HWSQN(d), Current TIME(u), Total Queued(d), Queuing TIME(u), P Buffer size(d), Status(d), Rate(d), Queue depth(u), retries(d), RSSI(d)
-     * 
-     */ 
+	time_wrap_counter=0;
 	while(
-/*
-	fscanf(f_input, "%d\t%d\t%d\t%d\t%d\t%d\t%d\n", 
-		&current.s_id, &s_sec, &s_usec,
-		&current.e_id, &e_sec, &e_usec, 
-		&current.e_size)  == 7 
-*/
-	fscanf(f_input, "%d\t%lf\t%d\t%lf\t%d\t%d\t%d\t%u\t%d\t%d\n", 
-		&current.s_id, &current.e_usec, &qt,
-		&current.s_usec,  &current.e_size, &status, 
-        &current.rate, &qd, &current.retries, &rssi)  >= 9
+	fscanf(f_input, "%d\t%u\t%d\t%u\t%d\t%d\t%d\t%u\t%d\t%d\n", 
+		&current.id, &current.return_t_usec, &current.total_queued,
+		&current.queuing_t_usec,  &current.packet_size, &current.status, 
+        &current.ratecode, &current.queue_depth, &current.lretry, &current.rssi
+        )  >= 10
 	){
-		// work out times in usec
-		/*
-		current.s_usec = s_usec + 1000000.0*s_sec;
-		current.e_usec = e_usec + 1000000.0*e_sec;
-		*/
-        /*
-	printf("%u\t%lf\t%d\t%lf\t%d\t%d\t%d\t%u\t%d\t%d\n", 
-		current.s_id, current.e_usec, qt,
-		current.s_usec,  current.e_size, status, 
-        rate, qd, retries, rssi);
-		*/
 		//Exclude first time through
-		if(last.s_id != -1){
+		if(last.id != -1){
 			// get service time
-			if(current.s_usec >= last.e_usec){
-				delay = current.e_usec - current.s_usec;
+			if(current.queuing_t_usec >= last.return_t_usec){
+				delay = current.return_t_usec - current.queuing_t_usec;
 			}
 			// fi-1 is greater
 			else{
-				delay = current.e_usec - last.e_usec;
+				delay = current.return_t_usec - last.return_t_usec;
 			}
-			//Only update values if correct packet size
-			//here we ignore other packets
 			/*
-			if(current.e_size == p_size){
-            */
+            * Print out stats for packets sent at a particular rate (24 == 11Mbps)
+            * If desired_ratecode == 0, print out stats for all  packets.
+            */ 
+			if(current.ratecode == desired_ratecode || desired_ratecode == 0 ){
 				if(delay < min_delay || min_delay == -1){
 					min_delay = delay;
 				}
 				service_average += delay;
-				queuing_average += current.e_usec - current.s_usec;
-                printf("%lf\t%lf\t%lf\n", current.s_usec, current.e_usec, queuing_average);
-				total_num_packets++;
+				queue_average += current.return_t_usec - current.queuing_t_usec;
+				//Only add successfully transmitted packets
+				//total_num_packets == successfully transmitted 
+				//packets. 
+				if(current.status == 0)
+					total_num_packets++;
+				//include failed packets here though
+				total_l_retries += current.lretry;
 				//add to appropriate bin
 				//New median bit, increment j until delay is less than
 				//max bin value, then add one to that bin
@@ -155,45 +139,55 @@ int qt=0, qd=0, status=0, rssi=0; //dummy variables for fscanf
 				data_bin[j][1]++;
 				j=0;
 				// Output to f_out file
-				fprintf(f_out, "%d\t%lf\t%lf\t%d\t%d\t%d\n",
-						current.s_id, current.e_usec-current.s_usec,
-						delay, current.e_size, current.retries,
-                        current.rate);
-				
-			//}
-			
+				fprintf(f_out, "%d\t%lf\t",
+						current.id, delay);
+				delay = current.return_t_usec - current.queuing_t_usec;
+				fprintf(f_out, "%lf\t%d\t%d\n",
+						delay,
+						current.lretry, current.packet_size);
+			} // packet size if() statement
 		}
-		//first run, record the start time (end time will be left in last.e_usec)
+		//first run, record the start time (end time will be left in last.return_t_usec)
 		else{
-			start_time = current.s_usec;
+			start_time = current.queuing_t_usec;
+			last_wrap_time = current.queuing_t_usec;
 			//printf("Start:%f\t", start_time);
 		}
+		// Need to watch for wrap around time for long runs! >4000 seconds
+		if(current.queuing_t_usec < last_wrap_time){
+			last_wrap_time = current.queuing_t_usec;
+			time_wrap_counter++;
+		}
 
-        /*
-        * Wraps
-        * 
-        if(current.e_usec < last.s_usec){
-            wrap_counter++;
-        }   
-        */ 
 		// Copy current into last and loop again
-		last.s_id = current.s_id;
-		last.e_id = current.e_id;
-		last.e_size = current.e_size;
-		last.s_usec = current.s_usec;
-		last.e_usec = current.e_usec;
-		last.retries = current.retries;
+		// last = current; // ?
+		last.id = current.id;
+		last.return_t_usec = current.return_t_usec;
+		last.total_queued = current.total_queued;
+		last.queuing_t_usec = current.queuing_t_usec;
+		last.packet_size = current.packet_size;
+		last.status = current.status;
+		last.ratecode = current.ratecode;
+		last.queue_depth = current.queue_depth;
+		last.lretry = current.lretry;
+		last.rssi = current.rssi;
 	}
+	//printf("End:%f\n", last.return_t_usec);
 	// work out averages
 	service_average = service_average/(float)total_num_packets;
-	queuing_average = queuing_average/(float)total_num_packets;
-	run_time = last.e_usec - start_time;
-    //run_time += wrap_counter * WRAP_32BIT_NUM;
+	queue_average = queue_average/(float)total_num_packets;
+	run_time = last.return_t_usec - start_time; // this doesn't account for wraps.
+	printf("Unadjusted RTIME:%f\t", run_time);
+	run_time += (double)time_wrap_counter * WRAP_TIME;
 	// this gives packets/usecond !
 	throughput = total_num_packets/run_time;
 	throughput *= 1000000; // for seconds
-	printf("Start:%f\tEnd:%f\tRun_time:%f\n", start_time, last.e_usec, run_time);
-	printf("Run time:%f\tPackets:%d\tThroughput:%f\n", run_time, total_num_packets, throughput);
+	col_prob = total_l_retries + total_num_packets;
+	col_prob = total_l_retries / col_prob;
+	//col_prob = total_num_packets + total_l_retries;
+	printf("Run_time(sec):%f\tWraps:%d\n", run_time/1000000, time_wrap_counter);
+	printf("Packets:%d\tThroughput(pps):%f\tP(col):%f\n", 
+			total_num_packets, throughput, col_prob);
 	
 		
 	/*
@@ -213,6 +207,7 @@ int qt=0, qd=0, status=0, rssi=0; //dummy variables for fscanf
 		else{
 			data_bin[j][2] = data_bin[j][0];
 		}
+		//printf("%d\t%d\t%d\n", data_bin[j][0], data_bin[j][1], data_bin[j][2]);
 		// Check for most populous bin ,(skip first bin)
 		if(j == 0){
 			pop_temp = data_bin[j][1];
@@ -236,18 +231,17 @@ int qt=0, qd=0, status=0, rssi=0; //dummy variables for fscanf
 	median = data_bin[bin_num][2]; // Want the (average for bin) delay value 
 
 	/*
-	 * Output to average file
+	 * Out put to average file
 	 */ 
 	fprintf(f_average,"%d\t%f\t%f\t%d\t%d\t%d\t%f\t", 
-			num_sta, service_average, queuing_average, median, 
+			num_sta, service_average, queue_average, median, 
 			min_delay, total_num_packets, throughput);
-	// Output throughput in Kbits/second (should be = 64Kb/s, for 80 byte packets)
+	// Output throughput in Kbits/second (should be = 64Kb/s, for 80 byte packets/100pps)
 	throughput *= (8*(float)real_p_size)/1000;
+	//throughput *= (8*(float)real_p_size)/1024;
 	printf("Throughput:%f Kb/s\n", throughput);
-	fprintf(f_average,"%f\n", throughput);
+	fprintf(f_average,"%f\t%f\n", throughput, col_prob);
 
-	
-	
 	// print out distribution of values
 	// format: bin num (i), MAX bin value, AVE bin value, Number in bin
 	for(i=0; i<M_BIN_NUM; i++){
@@ -255,8 +249,10 @@ int qt=0, qd=0, status=0, rssi=0; //dummy variables for fscanf
 	}
 
 	printf("Num:%d\tAve:%f\tCrude Ave:%f\tMedian:%d\n", 
-			num_sta, service_average, queuing_average, median);
+			num_sta, service_average, queue_average, median);
 	
+	
+// close all the files
 	
 fclose(f_input);
 fclose(f_average);
