ed243c935df20bf4a33910cb754d1bef004c11ef
Kfir Lavi <lavi.kfir@gmail.com>
2008-12-11 12:34:23 +0000

    ping logging functions are now in lib/ping

--- testbed/trunk/testbed/cgi-bin/gplot.dump.sh
+++ testbed/trunk/testbed/cgi-bin/gplot.dump.sh
@@ -10,80 +10,160 @@ source $LIB_PATH/plots
 # plot cwnd data reconstructed from tcpdump 
 input_file=$PATH_TRANSLATED
 
-[  -e $input_file ] \
-	&& tcpdump_file=$(io_uncompress_file $input_file)
+[ -e $input_file ] \
+	&& TCPDUMP_FILE=$(io_uncompress_file $input_file)
 
-FORMAT=$(cgi_query_string format $QUERY_STRING)
-STYLE2=$(cgi_query_string style2 $QUERY_STRING)
-PLOTNUM=$(cgi_query_string plot $QUERY_STRING)
-TITLE=$(cgi_query_string title $QUERY_STRING)
-XHI=$(cgi_query_string xhi $QUERY_STRING)
-YHI=$(cgi_query_string yhi $QUERY_STRING)
-XLO=$(cgi_query_string xlo $QUERY_STRING)
-YLO=$(cgi_query_string ylo $QUERY_STRING)
-STYLE=$(cgi_query_string style $QUERY_STRING)
-SIZE=$(cgi_query_string size $QUERY_STRING)
-NOTPUT=$(cgi_query_string tput $QUERY_STRING)
+get_query_string_params()
+{
+	FORMAT=$(cgi_query_string format $QUERY_STRING)
+	STYLE2=$(cgi_query_string style2 $QUERY_STRING)
+	PLOTNUM=$(cgi_query_string plot $QUERY_STRING)
+	TITLE=$(cgi_query_string title $QUERY_STRING)
+	XHI=$(cgi_query_string xhi $QUERY_STRING)
+	YHI=$(cgi_query_string yhi $QUERY_STRING)
+	XLO=$(cgi_query_string xlo $QUERY_STRING)
+	YLO=$(cgi_query_string ylo $QUERY_STRING)
+	STYLE=$(cgi_query_string style $QUERY_STRING)
+	SIZE=$(cgi_query_string size $QUERY_STRING)
+	NOTPUT=$(cgi_query_string tput $QUERY_STRING)
+	SINGLEPLOT=$(cgi_query_string singleplot $QUERY_STRING)
+}
 
-if [[ $FORMAT = "eps" ]]; then
-  FORMAT="postscript eps enhanced color"
-  MIME="Application/PostScript"
-fi
-if [[ $FORMAT = "" ]]; then
-	if [[ $SIZE -gt 0 ]]; then
-  		FORMAT="png size $(plots_get_zoom_plot_size $SIZE)"
-	else
-  		FORMAT="png size $(plots_get_plot_size)"
+set_format()
+{
+	if [[ $FORMAT = "eps" ]]; then
+	  FORMAT="postscript eps enhanced color"
+	  MIME="Application/PostScript"
 	fi
-  	
-  MIME="image/png"
-fi
+	if [[ $FORMAT = "" ]]; then
+		if [[ $SIZE -gt 0 ]]; then
+			FORMAT="png size $(plots_get_zoom_plot_size $SIZE)"
+		else
+			FORMAT="png size $(plots_get_plot_size)"
+		fi
+		
+	  MIME="image/png"
+	fi
+}
 
-if [[ $STYLE = "" ]]; then
-  STYLE="points"
-fi
+set_style()
+{
+	if [[ $STYLE = "" ]]; then
+	  STYLE="points"
+	fi
+}
 
-echo Content-type: $MIME 
-echo ""
+get_start_time()
+{
+	local tcpdump=$1
+	grep -v -i "#" $TCPDUMP_FILE \
+		| head -n 1 \
+		| cut -d ' ' -f 1
+}
 
-#start time
-starttime=`grep -v -i "#" $tcpdump_file | head -n 1 | cut -d ' ' -f 1`
-#first get list of unique destination ip and ports
-flow_ids=` grep -v -i "#" $tcpdump_file | cut -d ' ' -f 2 | sort | uniq `
+get_uniq_ip_and_ports_from_tcpdump()
+{
+	local dumpfile=$1 # tcp dump file
+	grep -v -i "#" $dumpfile \
+		| cut -d ' ' -f 2 \
+		| sort \
+		| uniq
+}
 
-#now sort output for gnuplot
-itemp=$(create_temp_file)
-temp=$(create_temp_file)
-tput=$(create_temp_file)
+generate_gnuplot_command_of_cwnd_tput_plot()
+{
+	local tcpdump_file=$1
+	local cwnd_plot_data=$2
+	local tput_plot_data=$3
+	local comma=
+	local plot_command='plot ' 
+	local j=0
+	for i in $(get_uniq_ip_and_ports_from_tcpdump $tcpdump_file); do
+	   grep -i $i $tcpdump_file >> $cwnd_plot_data
+	   echo -e '\n\n' >> $cwnd_plot_data
+	   plot_command="$plot_command $comma '$cwnd_plot_data' index $j u (\$1-$starttime):(\$3/1448) with $STYLE title 'flow $j cwnd' "
+	   if [[ ! $NOTPUT ]]; then
+	      plot_command="$plot_command, '$tput_plot_data' index $j u (\$1-$starttime):2 with $STYLE axes x1y2 title 'flow $j tput' "
+	   fi
+	   comma=","
+	   ((j++))
+	done
+	echo $plot_command
+}
 
-comma=''; plot='plot '; 
-j=0
-for i in $flow_ids; do
-   grep -i $i $tcpdump_file >> $temp
-   echo -e '\n\n' >>$temp
-   #plot="$plot $comma '$temp' index $j u (\$1-$starttime):(\$3/1448) with $STYLE title 'flow $j cwnd', '$itemp' index $j u 2:4 with $STYLE axes x1y2 title 'flow $j tput'"
-   if [[ $NOTPUT ]]; then
-      plot="$plot $comma '$temp' index $j u (\$1-$starttime):(\$3/1448) with $STYLE title 'flow $j cwnd' "
-   else
-      plot="$plot $comma '$temp' index $j u (\$1-$starttime):(\$3/1448) with $STYLE title 'flow $j cwnd', '$tput' index $j u (\$1-$starttime):2 with $STYLE axes x1y2 title 'flow $j tput' "
-   fi
-   comma=","
-   ((j++))
-done
+get_title_from_dumpfile()
+{
+	local dumpfile=$1
+	local title_number=$2
+	grep -i "#" $dumpfile \
+		| sed -n "${title_number}p" \
+		| sed -e 's/#//'
+}
 
-title1=`grep -i "#" $tcpdump_file | sed -n '1p' | sed -e 's/#//'`
-title2=`grep -i "#" $tcpdump_file | sed -n '2p' | sed -e 's/#//'`
+generate_ping_data()
+{
+	local rttfile=$1
+        awk '/time=/ {
+		split($0,pieces,"time="); 
+		split(pieces[2],pieces," "); 
+		print pieces[1]
+	}' $rttfile
+}
 
-if [  -e $rttfile ]; then
-	ping_inputfile=$(echo $input_file | sed -e 's/\.dump/\.ping/')
-	rttfile=$(io_uncompress_file $ping_inputfile)
-	rtt=$(create_temp_file)
-        awk '/time=/ {split($0,pieces,"time="); split(pieces[2],pieces," "); print pieces[1]} ' $rttfile > $rtt
-	#sed -e '1d' | cut -f 7 -d ' ' | cut -f 2 -d '=' >$rtt
-        plotping="plot \"$rtt\" u (\$0*1):1 with $STYLE title \"ping time\" "
-else
-        plotping=""
-fi
+generate_ping_plot_command()
+{
+	local ping_plot_command=
+	if [  -e $rttfile ]; then
+		ping_inputfile=$(echo $input_file | sed -e 's/\.dump/\.ping/')
+		rttfile=$(io_uncompress_file $ping_inputfile)
+		rtt_plot_data=$(create_temp_file)
+		generate_ping_data $rttfile > $rtt_plot_data
+		ping_plot_command="plot \"$rtt_plot_data\" u (\$0*1):1 with $STYLE title \"ping time\" "
+	fi
+	echo $ping_plot_command
+}
+
+generate_tput_plot_data()
+{
+	local cwnd_plot_data=$1
+	# get throughput from packet sequence numbers in tcpdump
+	awk --non-decimal-data \
+		'BEGIN {t=-1; dt=1; } {
+			if (NF < 3) 
+				printf "\n\n"; 
+			else {
+				seq=$4; 
+				if (t<0 || t>$1) {
+					t=$1; 
+					lastseq=seq; 
+					print $1, 0
+				}; 
+				if ($1-t >= dt) {
+					print $1, (seq-lastseq)*8/1024/1024/($1-t); 
+					t=$1; 
+					lastseq=seq
+				}; 
+			} 
+		}' $cwnd_plot_data
+}
+
+main()
+{
+	get_query_string_params
+	set_format
+	set_style
+	starttime=$(get_start_time $TCPDUMP_FILE)
+	echo Content-type: $MIME 
+	echo ""
+	cwnd_plot_data=$(create_temp_file)
+	tput_plot_data=$(create_temp_file)
+	plot=$(generate_gnuplot_command_of_cwnd_tput_plot $TCPDUMP_FILE $cwnd_plot_data $tput_plot_data)
+	generate_tput_plot_data $cwnd_plot_data > $tput_plot_data
+	title1=$(get_title_from_dumpfile $TCPDUMP_FILE 1)
+	title2=$(get_title_from_dumpfile $TCPDUMP_FILE 2)
+	ping_plot_command=$(generate_ping_plot_command)
+}
+main
 
 iperffile=`echo $input_file | sed -e 's/dump/iprf/'`
 [  -e $iperffile ] && iperf=$(io_uncompress_file $iperffile)
@@ -93,41 +173,24 @@ flow_ids=`grep -v -i "#" $iperf | cut -d ' ' -f 1 | sort | uniq `
 units=`cut -d ' ' -f 5 $iperf | head -n 1`
 
 ##now sort output for gnuplot
-comma=''; iplot='plot '; 
+iperf_plot_data=$(create_temp_file)
+comma=''; 
+iperf_plot_command='plot '; 
 j=0
 for i in $flow_ids; do
-   grep -i $i $iperf >> $itemp
-   echo -e '\n\n' >>$itemp
-   iplot="$iplot $comma '$itemp' index $j using 2:4 with $STYLE title 'flow $j tput'"
+   grep -i $i $iperf >> $iperf_plot_data
+   echo -e '\n\n' >>$iperf_plot_data
+   iplot="$iperf_plot_command $comma '$iperf_plot_data' index $j using 2:4 with $STYLE title 'flow $j tput'"
    comma=","
    ((j++))
 done
 
-# get throughput from packet sequence numbers in tcpdump
-awk --non-decimal-data \
-	'BEGIN {t=-1; dt=1; } {
-		if (NF < 3) 
-			printf "\n\n"; 
-		else {
-			seq=$4; 
-			if (t<0 || t>$1) {
-				t=$1; 
-				lastseq=seq; 
-				print $1, 0
-			}; 
-			if ($1-t >= dt) {
-				print $1, (seq-lastseq)*8/1024/1024/($1-t); 
-				t=$1; 
-				lastseq=seq
-			}; 
-		} 
-	}' $temp > $tput
 
-ititle1=`grep -i "#" $iperf | sed -n '1p' | sed -e 's/#//'`
-ititle2=`grep -i "#" $iperf | sed -n '2p' | sed -e 's/#//'`
+ititle1=$(get_title_from_dumpfile $iperf 1)
+ititle2=$(get_title_from_dumpfile $iperf 2)
 
-title1=`grep -i "#" $tcpdump_file | sed -n '1p' | sed -e 's/#//'`
-title2=`grep -i "#" $tcpdump_file | sed -n '2p' | sed -e 's/#//'`
+title1=$(get_title_from_dumpfile $TCPDUMP_FILE 1)
+title2=$(get_title_from_dumpfile $TCPDUMP_FILE 2)
 title3=$meantputs
 
 plott="plot [][0:1] 2"
@@ -135,16 +198,16 @@ MSTART="set size 2,2; set origin 0,0; set multiplot"
 MEND="unset multiplot"
 if [[ "$PLOTNUM" -eq 1 ]]; then
    iplot=""
-   plotping=""
+   ping_plot_command=""
    plott=""
    #MSTART=""
    #MEND=""
 elif [[ "$PLOTNUM" -eq 2 ]]; then
    plot=""
-   plotping=""
+   ping_plot_command=""
    plott=""
-   #MSTART=""
-   #MEND=""
+   MSTART=""
+   MEND=""
 fi
 
 if [[ $TITLE = "off" ]]; then
@@ -165,51 +228,86 @@ if [[ $NOTPUT ]]; then
 	YAXIS=""
 fi
 
-gnuplot <<- EOF
-	set terminal $FORMAT  
-	#set pointsize 0.3
-	set xlabel "time (s)"
-	set ylabel "cwnd (packets)"
-	set xrange [$XLO:$XHI]
-	set yrange [$YLO:$YHI]
-	#set y2label "throughput (Mbps)"
-	#set ytics nomirror
-	#set y2range [0:]
-	#set y2tics
-	$YAXIS
-	$MSTART
-	set title ""
+multi_plot()
+{
+	gnuplot <<- EOF
+		set terminal $FORMAT  
+		#set pointsize 0.3
+		set xlabel "time (s)"
+		set ylabel "cwnd (packets)"
+		set xrange [$XLO:$XHI]
+		set yrange [$YLO:$YHI]
+		#set y2label "throughput (Mbps)"
+		#set ytics nomirror
+		#set y2range [0:]
+		#set y2tics
+		$YAXIS
+		$MSTART
+		set title ""
 
-	set size 1, 0.95
-	set origin 0, 1 
-	$preamble
-	$plot
+		set size 1, 0.95
+		set origin 0, 1 
+		$preamble
+		$plot
 
-	#set size 1,0.95
-	set origin 1, 1
-	set ylabel "throughput (${units}bps)"
-	#set title "$ititle1 \n $ititle2"
-	$iplot
+		#set size 1,0.95
+		set origin 1, 1
+		set ylabel "throughput (${units}bps)"
+		#set title "$ititle1 \n $ititle2"
+		$iperf_plot_command
 
-	#set size 1,0.95
-	set origin 0, 0
-	set ylabel 'ping time (ms)'
-	$plotping
+		#set size 1,0.95
+		set origin 0, 0
+		set ylabel 'ping time (ms)'
+		$ping_plot_command
 
-	set size 2,0
-	set origin 0,2
-	set title "$title1 $title2"
-	unset border; unset key; unset xtics; unset ytics
-	unset xlabel; unset ylabel
-	#plot [][0:1] 2
-	$plott
+		set size 2,0
+		set origin 0,2
+		set title "$title1 $title2"
+		unset border; unset key; unset xtics; unset ytics
+		unset xlabel; unset ylabel
+		#plot [][0:1] 2
+		$plott
 
-	$MEND
-EOF
+		$MEND
+	EOF
+}
 
-release_temp_file $tcpdump_file 
-release_temp_file $temp 
+single_plot()
+{
+	local xlabel=$1
+	local ylabel=$2
+	gnuplot <<- EOF
+		set terminal $FORMAT  
+		set xlabel $xlabel
+		set ylabel $ylabel
+		set xrange [$XLO:$XHI]
+		set yrange [$YLO:$YHI]
+		set title ""
+		$preamble
+		$plot
+	EOF
+}
+
+case "$SINGLEPLOT" in
+	cwnd)
+		single_plot "time (s)" "cwnd (packets)"
+		;;
+	tput)
+		single_plot "time (s)" "cwnd (packets)"
+		;;
+	ping)
+		single_plot "time (s)" "cwnd (packets)"
+		;;
+	*)
+		multi_plot;
+		;;
+esac
+
+release_temp_file $TCPDUMP_FILE 
+release_temp_file $cwnd_plot_data 
+release_temp_file $tput_plot_data
 release_temp_file $rtt 
 release_temp_file $iperf 
-release_temp_file $itemp
+release_temp_file $iperf_plot_data
 
